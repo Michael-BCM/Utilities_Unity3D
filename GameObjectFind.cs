e8aa3fea77184161eacee3a14b880e420772c3d0
Michael-BCM <30367734+Michael-BCM@users.noreply.github.com>
2017-07-26 17:55:47 +0100

    GameObject.Find and GetComponent in one method.
    A function for finding a GameObject, placing it in an existing reference, and then retrieving any one of its components and placing that inside another existing reference.
    
    May be useful at runtime for finding and using prefabs/instantiated objects, and for retrieving their components for use. Use this method sparingly, ideally only on objects that are inaccessible during edit mode.

--- GameObjectFind.cs
+++ GameObjectFind.cs
@@ -1,13 +1,13 @@
 ﻿public class Utilities
 {
-    ///A function for finding a gameobject, placing it in a reference (created by you, the user),
-    ///and then retrieving any one of its components and placing that inside a reference as well, also created by you, the user.
+    ///A function for finding a GameObject, placing it in an existing reference, 
+    ///and then retrieving any one of its components and placing that inside another existing reference.
     public static void FindAndGetComponent<ComponentType>(ref UnityEngine.GameObject gameObject, 
         string objectName, ref ComponentType component) where ComponentType : UnityEngine.Component
     {
         gameObject = UnityEngine.GameObject.Find(objectName);
         component = gameObject.GetComponent<ComponentType>();
     }
-    ///Helpful at runtime for finding and using prefabs/instantiated objects, and for retrieving their components for use.
+    ///May be useful at runtime for finding and using prefabs/instantiated objects, and for retrieving their components for use. 
     ///Use this method sparingly, ideally only on objects that are inaccessible during edit mode. 
 }
