48ca477f54575d2afa75ee4cab67ddb7984f80ac
Michael <michael@bluecarbonmedia.co.uk>
2017-07-28 10:28:14 +0100

    Added sliders to randomise tile colours.

--- GlassTileParticles.cs
+++ GlassTileParticles.cs
@@ -11,8 +11,13 @@ public class GlassTileParticles : MonoBehaviour
     private float rotateIntv = 4F;
 
     [SerializeField]
-    private Color cubeColor;
+    private Color normalTileColor;
 
+    [SerializeField]
+    private bool enableRandomTileColors;
+    [SerializeField][Range(0, 1)]
+    private float redMin, redMax, greenMin, greenMax, blueMin, blueMax;
+    
     private void Start()
     {
         InvokeRepeating("changeX", 0, 0.1F);
@@ -20,8 +25,29 @@ public class GlassTileParticles : MonoBehaviour
         InvokeRepeating("changeZ", 0, 0.1F);
     }
 
-    void Update()
+    private Color tileColor ()
     {
+        if (!enableRandomTileColors)
+            return normalTileColor;
+
+        return new Color(Random.Range(redMin, redMax), Random.Range(greenMin, greenMax), Random.Range(blueMin, blueMax));
+    }
+
+    private void SetMinMaxSliders(ref float min, ref float max)
+    {
+        if (min > max)
+            min = max;
+
+        if (max < min)
+            max = min;
+    }
+
+    private void Update()
+    {
+        SetMinMaxSliders(ref redMin, ref redMax);
+        SetMinMaxSliders(ref greenMin, ref greenMax);
+        SetMinMaxSliders(ref blueMin, ref blueMax);
+
         if (Input.GetKeyDown(KeyCode.Space))
             canSpawn = !canSpawn;
 
@@ -29,7 +55,7 @@ public class GlassTileParticles : MonoBehaviour
         transform.Rotate(new Vector3(xRotation, yRotation, zRotation) * Time.deltaTime, Space.Self);
     }
 
-    void changeRotation(ref float rotationDegree)
+    private void changeRotation(ref float rotationDegree)
     {
         if (rotationDegree > -rotateSpeedLimit && rotationDegree < rotateSpeedLimit)
             rotationDegree += Random.Range(-rotateIntv, rotateIntv);
@@ -42,16 +68,15 @@ public class GlassTileParticles : MonoBehaviour
 
     }
 
-    void changeX() { changeRotation(ref xRotation); }
-    void changeY() { changeRotation(ref yRotation); }
-    void changeZ() { changeRotation(ref zRotation); }
+    private void changeX() { changeRotation(ref xRotation); }
+    private void changeY() { changeRotation(ref yRotation); }
+    private void changeZ() { changeRotation(ref zRotation); }
 
-    void SpawnCube()
+    private void SpawnCube()
     {
         if (!canSpawn)
-        {
             return;
-        }
+
         GameObject cube = Instantiate(prefab, transform.position, Quaternion.Euler(0, Random.Range(0, 90F), 0));
         cube.transform.localScale = new Vector3(Random.Range(0, 1F), Random.Range(0, 1F), Random.Range(0, 1F));
 
@@ -59,7 +84,7 @@ public class GlassTileParticles : MonoBehaviour
             Destroy(cube);
 
         Renderer r = cube.GetComponent<Renderer>();
-        r.material.color = cubeColor;
+        r.material.color = tileColor();
         r.material.SetFloat("_Metallic", Random.Range(0.7F, 1F));
         r.material.SetFloat("_Glossiness", Random.Range(0.7F, 1F));
         Destroy(cube, 10F);
