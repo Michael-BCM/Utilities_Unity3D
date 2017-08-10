361d9e2136d50ee2adae8af85d50b9cdab51b0e5
Michael-BCM <michael@bluecarbonmedia.co.uk>
2017-08-10 21:12:40 +0100

    Moved .cs files into Unity Project Assets folder

---
+++ Unity Utilities/Assets/Scripts/GlassTileParticles.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+public class GlassTileParticles : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefab;
+    bool canSpawn = false;
+
+    private float xRotation = 40F, yRotation = 40F, zRotation = 40F;
+    private float rotateSpeedLimit = 200F;
+    private float rotateIntv = 4F;
+
+    [SerializeField]
+    private Color normalTileColor;
+
+    [SerializeField]
+    private bool enableRandomTileColors;
+    [SerializeField][Range(0, 1)]
+    private float redMin, redMax, greenMin, greenMax, blueMin, blueMax;
+    
+    private void Start()
+    {
+        InvokeRepeating("changeX", 0, 0.1F);
+        InvokeRepeating("changeY", 0, 0.1F);
+        InvokeRepeating("changeZ", 0, 0.1F);
+    }
+
+    private Color tileColor ()
+    {
+        if (!enableRandomTileColors)
+            return normalTileColor;
+
+        return new Color(Random.Range(redMin, redMax), Random.Range(greenMin, greenMax), Random.Range(blueMin, blueMax));
+    }
+
+    private void SetMinMaxSliders(ref float min, ref float max)
+    {
+        if (min > max)
+            min = max;
+
+        if (max < min)
+            max = min;
+    }
+
+    private void Update()
+    {
+        SetMinMaxSliders(ref redMin, ref redMax);
+        SetMinMaxSliders(ref greenMin, ref greenMax);
+        SetMinMaxSliders(ref blueMin, ref blueMax);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            canSpawn = !canSpawn;
+
+        SpawnCube();
+        transform.Rotate(new Vector3(xRotation, yRotation, zRotation) * Time.deltaTime, Space.Self);
+    }
+
+    private void changeRotation(ref float rotationDegree)
+    {
+        if (rotationDegree > -rotateSpeedLimit && rotationDegree < rotateSpeedLimit)
+            rotationDegree += Random.Range(-rotateIntv, rotateIntv);
+
+        else if (rotationDegree <= -rotateSpeedLimit)
+            rotationDegree += rotateIntv;
+
+        else if (rotationDegree >= rotateSpeedLimit)
+            rotationDegree -= rotateIntv;
+
+    }
+
+    private void changeX() { changeRotation(ref xRotation); }
+    private void changeY() { changeRotation(ref yRotation); }
+    private void changeZ() { changeRotation(ref zRotation); }
+
+    private void SpawnCube()
+    {
+        if (!canSpawn)
+            return;
+
+        GameObject cube = Instantiate(prefab, transform.position, Quaternion.Euler(0, Random.Range(0, 90F), 0));
+        cube.transform.localScale = new Vector3(Random.Range(0, 1F), Random.Range(0, 1F), Random.Range(0, 1F));
+
+        if (cube.transform.localScale.x > 0.2 && cube.transform.localScale.y > 0.2 && cube.transform.localScale.z > 0.2)
+            Destroy(cube);
+
+        Renderer r = cube.GetComponent<Renderer>();
+        r.material.color = tileColor();
+        r.material.SetFloat("_Metallic", Random.Range(0.7F, 1F));
+        r.material.SetFloat("_Glossiness", Random.Range(0.7F, 1F));
+        Destroy(cube, 10F);
+    }
+}
---
+++ Unity Utilities/Assets/Scripts/JSONThingDemo.cs
@@ -0,0 +1,54 @@
+﻿/// A quick summary of a tutorial by GameGrind: https://www.youtube.com/watch?v=6yDRbnXve_0 (thanks!)
+/// on how to use the JSONUtility to save and load data to PC games and applications built using Unity3D.
+/// 
+/// GameGrind channel: https://www.youtube.com/channel/UCTY3kks3U4RDvpMX87fvo1A, like and subscribe to them to see more of their content. 
+/// 
+/// Summarised by me below. 
+
+using UnityEngine;
+using System.IO;
+
+public class JSONThingDemo : MonoBehaviour
+{
+    private string path;
+    private string jsonString;
+
+    private void Start ()
+    {
+        #region Load and print data to the console. 
+        path = Application.streamingAssetsPath + "/Thing.json"; 
+        //Sets up the path to read from, ending with the save file name. 
+        //Ensure that there is a 'StreamingAssets' folder in your Assets folder, and that there is a file in there with the above name. 
+
+        jsonString = File.ReadAllText(path);
+        //Opens said save file, reads the file contents into a string 'jsonString', then closes the file.
+
+        Thing Something = JsonUtility.FromJson<Thing>(jsonString);
+        //Creates a new instance of the 'Thing' class, and writes the contents of the string into the class. 
+
+        print(Something.Name + ", " + Something.Level);
+        //The lines taken from the json file, once written to the class, are displayed in the console.
+        #endregion
+
+        #region Modify and save data to the JSON file. 
+        Something.Level = 3146;
+        //Sets the 'level' statistic to something different. 
+
+        string newSomething = JsonUtility.ToJson(Something);
+        //Takes the modified data and generates a json representation of it. 
+
+        File.WriteAllText(path, newSomething);
+        //Writes the json representation back to the original file.
+        #endregion
+
+        print(newSomething);
+    }
+}
+
+[System.Serializable]
+public class Thing
+{
+    public string Name;
+    public int Level;
+    public int[] Stats;
+}
